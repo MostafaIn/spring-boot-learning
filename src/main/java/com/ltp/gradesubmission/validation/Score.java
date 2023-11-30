7be00def1cc7231566a74b4acd67be4b9aae64bc
Mostafa Hazareh <mostafa2015se@gmail.com>
2023-11-30 16:20:41 +0200

    - Initial commit. - Learn testing.

--- src/main/java/com/ltp/gradesubmission/validation/Score.java
+++ src/main/java/com/ltp/gradesubmission/validation/Score.java
@@ -1,17 +1,20 @@
-package com.ltp.gradesubmission;
+package com.ltp.gradesubmission.validation;
 
-import javax.validation.Constraint;
-import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = ScoreValidator.class)
 public @interface Score {
-    String message() default  "Invalid data!";
-    Class<?>[] groups() default {};
+
+    String message() default "Invalid Data";
+	Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+
 }
