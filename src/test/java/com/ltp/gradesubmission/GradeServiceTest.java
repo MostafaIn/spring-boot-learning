0bffa0a5d11bae52e9ef98fa4c5604bd8136fe07
Mostafa Hazareh <mostafa2015se@gmail.com>
2023-11-30 17:07:20 +0200

    - @Test tells JUnit to run the method as a test. - Three steps to writing a unit test: -- Arrange: Mock the data needed to carry out the unit test. -- Act: Call the method that you want to test. -- Assert: Check if the method is behaving correctly.

--- src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
+++ src/test/java/com/ltp/gradesubmission/GradeServiceTest.java
@@ -1,11 +1,18 @@
 package com.ltp.gradesubmission;
 
+import com.ltp.gradesubmission.pojo.Grade;
 import com.ltp.gradesubmission.repository.GradeRepository;
 import com.ltp.gradesubmission.service.GradeService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GradeServiceTest {
@@ -15,4 +22,20 @@ public class GradeServiceTest {
 
     @InjectMocks
     private GradeService gradeService;
+
+    @Test
+    public void getGradesFromTepoTest(){
+        // ARRANGE
+        when(gradeRepository.getGrades()).thenReturn(Arrays.asList(
+                new Grade("Mosy","Spring-boot","C-"),
+                new Grade("Rohy","Radio Chemistry","A+")
+        ));
+
+        //ACT
+        List<Grade> result = gradeService.getGrades();
+
+        //ASSERT
+        assertEquals("Mosy", result.get(0).getName());
+        assertEquals("Radio Chemistry", result.get(1).getSubject());
+    }
 }
