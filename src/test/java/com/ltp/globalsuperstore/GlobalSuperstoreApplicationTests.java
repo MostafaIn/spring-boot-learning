3e4321fcae9cbbd8a88fc7cfdc5ee80f03997606
Mostafa Hazareh <mostafa2015se@gmail.com>
2023-11-30 14:58:35 +0200

    - Initial commit for challenge5. - Challenge5: Autowired VS Constructor.

--- src/test/java/com/ltp/globalsuperstore/GlobalSuperstoreApplicationTests.java
+++ src/test/java/com/ltp/globalsuperstore/GlobalSuperstoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ltp.gradesubmission;
+package com.ltp.globalsuperstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GradeSubmissionApplicationTests {
+class GlobalSuperstoreApplicationTests {
 
 	@Test
 	void contextLoads() {
