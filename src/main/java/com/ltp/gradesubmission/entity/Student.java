d6f3e424622c7142cde3c1a8953b8708a472df59
Mostafa Hazareh <mostafa2015se@gmail.com>
2023-12-04 14:56:09 +0200

    - Set up a Student @Entity. - Each @Column field maps to a table column.

--- src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -1,11 +1,20 @@
 package com.ltp.gradesubmission.entity;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="student")
 public class Student {
 
+    @Column(name="id")
     private Long id;
+    @Column(name="name", nullable = false)
     private String name;
+    @Column(name="birth_date", nullable = false)
     private LocalDate birthDate;
 
     public Long getId() {
