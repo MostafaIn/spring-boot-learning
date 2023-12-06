b66a9533e84f9b9fb664137c6c1d6b355c9512a9
Mostafa Hazareh <mostafa2015se@gmail.com>
2023-12-06 14:07:37 +0200

    - Setup grade entity.

--- src/main/java/com/ltp/gradesubmission/entity/Grade.java
+++ src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -2,13 +2,23 @@ package com.ltp.gradesubmission.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Entity
+@Table(name="grade")
+
 public class Grade {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "score", nullable = false)
     private String score;
 
 }
