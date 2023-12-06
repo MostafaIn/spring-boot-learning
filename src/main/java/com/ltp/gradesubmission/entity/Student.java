ae61e99880e50b189fdfad5cd9fbb3dc4862fb78
Mostafa Hazareh <mostafa2015se@gmail.com>
2023-12-06 13:21:43 +0200

    - Lombok. -- Using @Getter and @Setter to auto generate getter and setter. -- Using @AllArgsConstructor to auto generate a constructor for all data in this class. -- Using @NoArgsConstructor to auto generate an empty constructor.

--- src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -1,11 +1,17 @@
 package com.ltp.gradesubmission.entity;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name="student")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
 
     @Id
@@ -16,29 +22,5 @@ public class Student {
     private String name;
     @Column(name="birth_date", nullable = false)
     private LocalDate birthDate;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return this.birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
 
 }
