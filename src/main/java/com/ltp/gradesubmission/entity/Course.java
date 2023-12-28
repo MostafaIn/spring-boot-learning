3ecdc0dd9e383bf8a188074f4137e71dabdf554d
Mostafa Hazareh <mostafa2015se@gmail.com>
2023-12-28 14:07:28 +0200

    - Course Entity.

--- src/main/java/com/ltp/gradesubmission/entity/Course.java
+++ src/main/java/com/ltp/gradesubmission/entity/Course.java
@@ -3,15 +3,28 @@ package com.ltp.gradesubmission.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @Column(name = "id")
     private Long id;
+    @NonNull
+    @Column(name = "subject", nullable = false)
     private String subject;
+    @NonNull
+    @Column(name = "code", nullable = false)
     private String code;
+    @NonNull
+    @Column(name = "description", nullable = false)
     private String description;
 
 }
